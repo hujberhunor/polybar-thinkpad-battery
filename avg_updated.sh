de942872b48c9823dcb19e7f81a19d7d7e096e8f
Hujber Hunor <50179148+hujberhunor@users.noreply.github.com>
2025-01-30 14:26:40 +0100

    Updated with dynamic icon
    avg-updated is now displays the battery icon according to the current charge.

--- avg_updated.sh
+++ avg_updated.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
-## This script is handled by POLYBAR config
 
 # Get the current battery levels
 bat0=$(cat /sys/class/power_supply/BAT0/capacity) # INTERNAL
 bat1=$(cat /sys/class/power_supply/BAT1/capacity) # EXTERNAL
 
 # Get the wh counts of the batteries
-bat0_wh=$(cat /sys/class/power_supply/BAT0/energy_full_design)/1000000
-bat1_wh=$(cat /sys/class/power_supply/BAT1/energy_full_design)/1000000
+bat0_wh=$(($(cat /sys/class/power_supply/BAT0/energy_full_design) / 1000000))
+bat1_wh=$(($(cat /sys/class/power_supply/BAT1/energy_full_design) / 1000000))
 
 # Define the weights for each battery capacity 
 weight1=24  # internal
 weight2=72  # external 
 
 # Determine the calculation method based on wh values
-# If both batteries wh is less then 25(wh) then use SIMPLE avg calculation
-# ELSE use the WEIGHTED method
 if [[ "$bat0_wh" -le 25 && "$bat1_wh" -le 25 ]]; then
-  # Both batteries are around 24000000, use non-weighted average
   average=$(( (bat0 + bat1) / 2 ))
 else
-  # At least one battery is larger (around 71280000), use weighted average
   total_weight=$((weight1 + weight2))
   average=$(( (bat0 * weight1 + bat1 * weight2) / total_weight ))
 fi
@@ -45,20 +40,27 @@ fi
 # Get the AC adapter status
 ac_status=$(cat /sys/class/power_supply/AC/online)
 
-# Output the battery levels, charging status, and AC adapter status
-## Charging and AC is connected -> Green display
-if [ "$charging" == true ] && [ "$ac_status" == 1 ]; then
-  echo "%{F#00ff00}↑%{F-}%{F#00ff00}%{F-}  $average% $bat_watt AC" 
-
-## Not charging and AC is connected (so batteries at threshold) -> White display
-elif [ "$charging" == false ] && [ "$ac_status" == 1 ]; then
-  echo "%{F#FFFFFF}%{F-} $average% AC"  
-
-## Average battery are under 26% -> Red display
-elif [ "$average" -lt 16 ]; then
-  echo "%{F#FF0000}%{F-} $average% $bat_watt"  
-
-## Else discharging
+# Select battery icon dynamically
+if [ "$average" -le 10 ]; then
+  battery_icon="" # Empty
+elif [ "$average" -le 30 ]; then
+  battery_icon="" # Low
+elif [ "$average" -le 60 ]; then
+  battery_icon="" # Medium
+elif [ "$average" -le 90 ]; then
+  battery_icon="" # High
 else
-  echo "%{F#FFAA00}%{F-} $average% $bat_watt" 
+  battery_icon="" # Full
 fi
+
+# Output the battery levels, charging status, and AC adapter status
+if [ "$charging" == true ] && [ "$ac_status" == 1 ]; then
+  echo "%{F#00ff00}↑%{F-}%{F#00ff00}$battery_icon%{F-}  $average% $bat_watt AC" 
+elif [ "$charging" == false ] && [ "$ac_status" == 1 ]; then
+  echo "%{F#FFFFFF}$battery_icon%{F-} $average% AC"  
+elif [ "$average" -lt 16 ]; then
+  echo "%{F#FF0000}$battery_icon%{F-} $average% $bat_watt"  
+else
+  echo "%{F#FFAA00}$battery_icon%{F-} $average% $bat_watt" 
+fi
+
